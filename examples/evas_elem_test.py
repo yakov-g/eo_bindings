83eafe8424d1b7f66f7e3572b588ddce0a10f2fc
Yakov Goldberg <yakov.g@samsung.com>
2012-11-15 18:07:58 +0200

    example update
    Signed-off-by: Yakov Goldberg <yakov.g@samsung.com>

--- examples/evas_elem_test.py
+++ examples/evas_elem_test.py
@@ -1,10 +1,40 @@
 # -*- coding: UTF-8 -*-
 import sys
 
-#from base.eodefault import py_elm_init, elem_runi
+from base.eodefault import py_elm_init, elem_run
 from eobase import EoBase
 #from evas_lib.py.evas_l import Evas, EvasObjectRectangle, EvasObjectLine
-from evas_elem_l.py.evas_elem_py import Evas, EvasObjectRectangle, EvasObjectLine, ElmWin
+from evas_elem_l.py.evas_elem_py import Evas, EvasObjectRectangle, EvasObjectLine, ElmWin, ElmBg
+
+
+print "Initializing Elementary..."
+print "Init res:", py_elm_init(sys.argv)
+print ""
+
+
+w1 = ElmWin(None, "my win name", 0)
+w1.title_set("first_title")
+t = w1.title_get()
+print "title:", t
+w1.size_set(370, 350)
+xx,yy = w1.size_get()
+print "size : " ,  xx, yy
+w1.visibility_set(1)
+v = w1.visibility_get()
+print "v=", v
+
+bg = ElmBg(w1)
+bg.size_hint_weight_set(1.0, 1.0)
+w1.resize_object_add(bg)
+bg.visibility_set(1)
+
+
+
+
+
+elem_run()
+"""
+
 
 ev = Evas(None)
 rect = EvasObjectRectangle(ev)
@@ -19,5 +49,5 @@ line.xy_set(0, 0, 10, 20)
 r, g, b, a = line.xy_get()
 print "%d %d %d %d"%(r, g, b, a)
 print "=================="
-
+"""
 
