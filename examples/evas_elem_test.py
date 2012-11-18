83a457b6c564d6c404275e79638586d446ba1eaf
Yakov Goldberg <yakov.g@samsung.com>
2012-11-18 12:21:04 +0200

    Examples updated
    Signed-off-by: Yakov Goldberg <yakov.g@samsung.com>

--- examples/evas_elem_test.py
+++ examples/evas_elem_test.py
@@ -2,15 +2,13 @@
 import sys
 
 from base.eodefault import py_elm_init, elem_run
-from eobase import EoBase
+#from eobase import EoBase
 #from evas_lib.py.evas_l import Evas, EvasObjectRectangle, EvasObjectLine
-from evas_elem_l.py.evas_elem_py import Evas, EvasObjectRectangle, EvasObjectLine, ElmWin, ElmBg
-
+from evas_elem_l.py.evas_elem_py import ElmWin, ElmBg, ElmBox, ElmButton
 
 print "Initializing Elementary..."
 print "Init res:", py_elm_init(sys.argv)
 print ""
-
 
 w1 = ElmWin(None, "my win name", 0)
 w1.title_set("first_title")
@@ -29,13 +27,19 @@ w1.resize_object_add(bg)
 bg.visibility_set(1)
 
 
+box = ElmBox(w1)
+box.size_hint_weight_set(1.0, 1.0)
+w1.resize_object_add(box)
+box.visibility_set(1)
 
-
+but = ElmButton(w1)
+#but.text_set("Button")
+box.pack_end(but)
+but.visibility_set(1)
 
 elem_run()
+
 """
-
-
 ev = Evas(None)
 rect = EvasObjectRectangle(ev)
 line = EvasObjectLine(ev)
